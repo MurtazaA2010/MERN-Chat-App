d3bd4a1bd7b51e2b0f0da2513ac04cc7b1dc384b
Murtaza Abdullah <140527812+MurtazaA2010@users.noreply.github.com>
2024-05-01 10:08:56 +0600

    Update auth.routes.js

--- api /Routes /auth.routes.js
+++ api /Routes /auth.routes.js
@@ -1,1 +1,11 @@
+import express from "express";
+import {signup, signin, signout} from '../controller/auth.js'
+const router = express.Router();
 
+router.post("/signup", signup) 
+
+router.post("/signin",signin) 
+
+router.post("/signout" ,signout) 
+
+export default router;
