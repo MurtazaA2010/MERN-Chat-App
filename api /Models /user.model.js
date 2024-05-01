28b64e02ac9b7764372eb27973060efc26183937
Murtaza Abdullah <140527812+MurtazaA2010@users.noreply.github.com>
2024-05-01 10:08:33 +0600

    Update user.model.js

--- api /Models /user.model.js
+++ api /Models /user.model.js
@@ -1,1 +1,29 @@
+import mongoose from "mongoose";
 
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    "fullname" : {
+        type : String,
+        required : true
+    },
+    "username": {
+        type : String,
+        required : true,
+        minlength: 5,
+        maxlength : 30,
+        unique: true
+    },
+    "password" : {
+        type : String,
+        required : true,
+    },
+    "profilePic" : {
+        type: String,
+        default : ""
+    } 
+})
+
+const User = mongoose.model('User', UserSchema);
+
+export default User;
