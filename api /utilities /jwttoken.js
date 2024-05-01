ccab811e0ba5dcc054ed0f6355050ffdb8a0350c
Murtaza Abdullah <140527812+MurtazaA2010@users.noreply.github.com>
2024-05-01 10:09:32 +0600

    Update jwttoken.js

--- api /utilities /jwttoken.js
+++ api /utilities /jwttoken.js
@@ -1,1 +1,16 @@
+import jwt from "jsonwebtoken";
 
+const genTokenAndSetCookie = (userId, res)=> {
+    const token = jwt.sign({userId}, process.env.JWT || "up60a9RS9AsspdDYG4mvcGLshs59xQ7dYmidSuQNUV8=", {
+        expiresIn : "30d"
+    }
+    ) 
+
+    res.cookie(jwt, token, {
+        maxAge : 30*24*60*60*1000,
+        httpOnly : true,
+        sameSite : "strict",
+    })
+}
+
+export default genTokenAndSetCookie;
