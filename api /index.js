88060ebcf2182797e0acfb82da250116a1bce0b5
Murtaza Abdullah <140527812+MurtazaA2010@users.noreply.github.com>
2024-05-06 21:59:35 +0600

    Update index.js

--- api /index.js
+++ api /index.js
@@ -2,9 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./Routes/auth.routes.js";
 import mongoose from "mongoose";
+import cookieParser  from "cookie-parser";
+import messageRoutes from "./Routes/messages.routes.js";
+import userRoutes from "./Routes/user.routes.js"
 
 const app = express();
-
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
@@ -19,10 +21,13 @@ mongoose.connect(mongodb).then(()=> {
 })
 
 app.use(express.json()); 
+app.use(cookieParser());
 
 // app.use((req,res, next) => {
 //     console.log(req.url)
 //     next()
 // })
 
-app.use("/api/auth" , authRoutes)
+app.use("/api/auth" , authRoutes);
+app.use('/api/messages', messageRoutes);
+app.use('/api/users', userRoutes)
