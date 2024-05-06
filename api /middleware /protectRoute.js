4b53e1f45400a623e76c0738b3f2ab3044480c8e
Murtaza Abdullah <140527812+MurtazaA2010@users.noreply.github.com>
2024-05-06 22:00:49 +0600

    Update protectRoute.js

--- api /middleware /protectRoute.js
+++ api /middleware /protectRoute.js
@@ -1,1 +1,30 @@
+import jwt from "jsonwebtoken";
+import User from "../Models/user.model.js";
+
+const protectRoute = async (req, res, next) => {
+    try {
+        const token = req.cookies.jwt; // Check for token in cookies
+        if (!token) {
+            return res.status(401).json({ error: "Unauthorized: No token provided. Please sign in or sign up first." });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT|| "default_secret_key");
+        if (!decoded) {
+            return res.status(401).json({ error: "Unauthorized: Invalid token." });
+        }
+
+        const user = await User.findById(decoded.userId).select('-password');
+        if (!user) {
+            return res.status(401).json({ error: "User not found." });
+        }
+
+        req.user = user; // Attach user object to request
+        next();
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+export default protectRoute;
 
